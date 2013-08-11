2654c5eb92688e1f71716fd1b3f929ac014acba7
vade <vade@vade.info>
2013-08-11 00:37:44 -0400

    Working initial commit of Rift sensor plugin. Outputs live data from the first available rift attached.

---
+++ v002 Oculus Rift/v002 Oculus Rift/v002_Oculus_RiftPlugIn.h
@@ -0,0 +1,17 @@
+//
+//  v002_Oculus_RiftPlugIn.h
+//  v002 Oculus Rift
+//
+//  Created by vade on 8/5/13.
+//  Copyright (c) 2013 v002. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface v002_Oculus_RiftPlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
